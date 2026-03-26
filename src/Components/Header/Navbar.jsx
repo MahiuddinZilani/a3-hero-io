05b867acd208f2fa5b13baa7bfd4fa2debae2cb8
MahiuddinZilani <zilani.cse.cou@gmail.com>
2026-03-26 23:58:11 +0600

    final commit

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,25 +1,37 @@
-import { House, HouseIcon } from "lucide-react";
 import React from "react";
-import { Link } from "react-router";
+import { NavLink } from "react-router";
+import logo from "../../assets/logo.png";
+import { Home } from "lucide-react";
 
 const Navbar = () => {
+  const linkClass = ({ isActive }) =>
+    isActive ? "font-semibold border-b-2 border-primary " : "hover:font-bold";
+
   const links = (
     <>
-      <Link to="/" className="mr-2">
-        <li>Home</li>
-      </Link>
-      <Link to="/apps" className="mr-2">
-        <li>Apps</li>
-      </Link>
-      <Link to="/installation" className="mr-2">
-        <li>Installation</li>
-      </Link>
+      <li className="mx-2">
+        <NavLink to="/" className={linkClass}>
+          <img src="./icon/home-icon.svg" alt="" />
+          Home
+        </NavLink>
+      </li>
+      <li className="mx-2">
+        <NavLink to="/apps" className={linkClass}>
+          <img src="./icon/apps-icon.svg" alt="" />
+          Apps
+        </NavLink>
+      </li>
+      <li className="mx-2">
+        <NavLink to="/installation" className={linkClass}>
+          <img src="./icon/installation-icon.svg" alt="" />
+          Installation
+        </NavLink>
+      </li>
     </>
   );
 
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      {/* navbar start */}
+    <div className="navbar max-w-7xl mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -30,31 +42,33 @@ const Navbar = () => {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              {" "}
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
                 d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
+              />
             </svg>
           </div>
+
           <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            tabIndex={-1}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
           >
             {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+
+        <img src={logo} alt="logo" className="w-10" />
+        <span className="btn btn-ghost text-xl">Hero Store</span>
       </div>
-      {/* navbar center  */}
+
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
-      {/* navbar end  */}
+
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <button className="btn btn-primary">Contribute</button>
       </div>
     </div>
   );
